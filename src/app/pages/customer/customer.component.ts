aa05a826df8a5a60216058e2febf3581b95926e0
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-10-16 11:30:58 -0300

    refactor: modificação de metodo

--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from 'src/app/services/customer.service';
 import { Customer } from 'src/app/model/customer';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer',
@@ -11,16 +10,9 @@ import { Router } from '@angular/router';
 export class CustomerComponent implements OnInit {
   customer: Customer | null = null;
 
-  constructor(private customerService: CustomerService, private router: Router) { }
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit() {
     this.customer = this.customerService.getSelectedCustomer();
-  }
-
-  saveCustomerChanges() {
-    if (this.customer) {
-      this.customerService.saveCustomer(this.customer);
-      this.router.navigate(['/home']);
-    }
   }
 }
