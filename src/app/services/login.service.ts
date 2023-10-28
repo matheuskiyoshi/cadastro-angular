6adbfac5617b0871443d1b251f19f929876f6232
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-10-27 21:19:37 -0300

    style: modificação de componentes para bootstrap

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -7,8 +7,8 @@ export class LoginService {
 
   constructor() { }
 
-  private email = 'matheus@gmail.com';
-  private senha = '123456';
+  private email = 'admin@admin.com';
+  private senha = 'admin123';
 
   validateLogin(inputEmail: string, inputPassword: string): boolean{
     return inputEmail === this.email && inputPassword === this.senha;
